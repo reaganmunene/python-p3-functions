dd6e75d0b4df23bb5245e8cab9cf206f8451d88f
reaganmunene <reagannmunene@gmail.com>
2024-04-06 01:24:21 +0300

    completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,21 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+ print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
+    sum = num1 + num2
+    return sum
     pass
 
 def halve(number):
+    half = number/2
+    return half
     pass
